daacf9ec74cc92e222a5b05b07e2f4aff456692b
zafkielkurumi <563693600@qq.com>
2021-12-31 17:43:52 +0800

    Feat::sparkles: 回响排序

--- src/app/pages/home/component/arsenal/arsenal.component.ts
+++ src/app/pages/home/component/arsenal/arsenal.component.ts
@@ -1,48 +1,48 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Core } from '@src/app/model/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { CoreElement, CoreSeries } from '@src/app/constants/enum';
 import { outCoreList } from './outCore';
-import { CdkDragDrop, CdkDragStart, CdkDragMove } from '@angular/cdk/drag-drop'
+import { CdkDragDrop, CdkDragStart, CdkDragMove } from '@angular/cdk/drag-drop';
 import { CoreOption } from '@src/app/constants/contants';
 
 @Component({
   selector: 'app-arsenal',
   templateUrl: './arsenal.component.html',
-  styleUrls: ['./arsenal.component.scss']
+  styleUrls: ['./arsenal.component.scss'],
 })
 export class ArsenalComponent implements OnInit {
   coreList: Core[] = [];
   currentCoreElement = CoreElement.风;
   coreOption = CoreOption;
 
-  constructor(
-    private _http: HttpClient
-  ) { }
+  constructor(private _http: HttpClient) {}
 
   ngOnInit(): void {
-    this._http.get<Core[]>('/assets/core.json').subscribe(r => {
-      this.coreList = r.filter(core => !outCoreList.includes(core.id));
+    this._http.get<Core[]>('/assets/core.json').subscribe((r) => {
+      this.coreList = r.filter((core) => !outCoreList.includes(core.id));
     });
   }
 
   get currentCoreList() {
-    return this.coreList.filter(core => core.data.coreElement === this.currentCoreElement)
+    return this.coreList.filter((core) => core.data.coreElement === this.currentCoreElement);
   }
 
-
   get magunaCore() {
-    return this.currentCoreList.filter(core => core.data.coreSeries === CoreSeries.maguna)
+    return this.currentCoreList
+      .filter((core) => core.data.coreSeries === CoreSeries.maguna)
+      .sort((a, b) => b.data.coreRarity - a.data.coreRarity);
   }
 
   get normalCore() {
-    return this.currentCoreList.filter(core => core.data.coreSeries === CoreSeries.normal)
+    return this.currentCoreList
+      .filter((core) => core.data.coreSeries === CoreSeries.normal)
+      .sort((a, b) => b.data.coreRarity - a.data.coreRarity);
   }
 
   get exCore() {
-    return this.currentCoreList.filter(core => core.data.coreSeries === CoreSeries.ex)
+    return this.currentCoreList
+      .filter((core) => core.data.coreSeries === CoreSeries.ex)
+      .sort((a, b) => b.data.coreRarity - a.data.coreRarity);
   }
-
 }
-
-
