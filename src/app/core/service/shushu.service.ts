ca0588711d9ceee37a6db7c9fc349d7925d23a3b
zafkielkurumi <563693600@qq.com>
2022-01-24 23:58:07 +0800

    Feat::sparkles: 更新浑身和背水曲线公式

--- src/app/core/service/shushu.service.ts
+++ src/app/core/service/shushu.service.ts
@@ -23,25 +23,28 @@ export class ShushuService {
 
   /**
    * 根据背水曲线公式计算当前hp的背水攻刃加成 80%hp以上为0
-   * factor 为当前剩余血量百分比
+   * X 为当前剩余血量百分比
+   * 1.3X^2-3.75X+2.55
    */
   enmityWithHp(hp: number) {
     if (hp >= 80) {
       return 0;
     }
-    const factor = 1 - hp / 100;
-    return (1 + 2 * factor) * factor;
+    const X = 1 - hp / 100;
+    return 1.3 * X * X - 3.75 * X + 2.55;
   }
 
   /**
    * 根据浑身公式计算当前hp的浑身加成 25%以下为0
-   * 浑身公式暂不明确 从容效果 = ( 2.1 + ( r /从容系数 ) ^ 2.9 ) / 100
-   * r 为当前血量百分比
+   * 浑身公式暂不明确
+   * X 为当前血量百分比
+   * 0.99X^2+0.075X+0.01
    */
   staminaWithHp(hp: number) {
     if (hp <= 25) {
       return 0;
     }
-    return 1;
+    const X = hp / 100;
+    return 0.99 * X + 0.075 * X + 0.01;
   }
 }
